819b59d4363e16e0f9df18923be3f6094048008b
Galen <galen.dively4@gmail.com>
2026-01-28 10:51:32 -0500

    Week2 Script

---
+++ week2/script.ps1
@@ -0,0 +1,7 @@
+﻿$chrome = Get-Process -Name "chrome" -ErrorAction SilentlyContinue
+
+if ($chrome) {
+    Stop-Process -Name "chrome" -Force
+} else {
+    Start-Process chrome "https://www.champlain.edu"
+}
