4c1fd972d57085f2d9c4bfd1292f0c73b3d6c8de
Galen <galen.dively4@gmail.com>
2026-02-11 14:52:28 -0500

    week 5

---
+++ week5/main.py.ps1
@@ -0,0 +1,27 @@
+﻿.  C:\Users\champuser\SYS320-01\week5\webscrape.ps1
+
+#daysTranslator(gatherClasses) | 
+    #Select-Object "Class Code", Instructor, Location, Days, "Time Start", "Time End" | 
+    #Where-Object {$_.Instructor -ilike "*Furkan*"}
+
+
+$FullTable = daysTranslator(gatherClasses)
+
+#$FullTable | Where-Object {($_.Location -ilike "FREE 105") -and ($_.Days -ilike "Wednesday") } | 
+ #   Sort-Object "Time Start" |
+  #  Select-Object "Time Start", "Time End", "Class Code"
+
+
+$ITSInstructors = $FullTable | Where-Object {($_."Class Code" -ilike "SYS*") -or 
+                                             ($_."Class Code" -ilike "NET*") -or
+                                             ($_."Class Code" -ilike "SEC*") -or
+                                             ($_."Class Code" -ilike "FOR*") -or
+                                             ($_."Class Code" -ilike "CSI*") -or
+                                             ($_."Class Code" -ilike "DAT*")} |
+                                             Select-Object "Instructor" -Unique |
+                                             Sort-Object "Instructor"
+
+# $ITSInstructors
+
+$FullTable | Where {$_.Instructor -in $ITSInstructors.Instructor } | Group "Instructor" | Select Count,Name | Sort-Object Count -Descending
+    
---
+++ week5/webscrape.ps1
@@ -0,0 +1,41 @@
+﻿function gatherClasses(){
+    $page = Invoke-WebRequest -TimeoutSec 2 "http://127.0.0.1/Courses2026SP.html"
+    $trs = $page.ParsedHTML.body.getElementsByTagName("tr")
+
+    $FullTable = @()
+    for($i=1; $i -lt $trs.length; $i++){
+        $tds = $trs[$i].getElementsByTagName("td")
+        $times = $tds[5].innerText.split("-")
+
+        $FullTable += [pscustomobject]@{"Class Code" = $tds[0].innerText;
+                                        "Title" = $tds[1].innerText;
+                                         "Days" = $tds[4].innerText;
+                                         "Time Start" = $times[0];
+                                         "Time End" = $times[1];
+                                         "Instructor" = $tds[6].innerText;
+                                         "Location" = $tds[9].innerText;
+                                         }
+
+  
+    }
+  return $FullTable
+}
+
+
+function daysTranslator($FullTable){
+    for($i=0; $i -lt $FullTable.length; $i++){
+
+        $Days = @()
+
+        if ($FullTable[$i].Days -ilike "M*") {$Days += "Monday"}
+        if($FullTable[$i].Days -ilike "*[TWF]*") {$Days += "Tuesday"}
+        elseif($FullTable[$i].Days -ilike "T") {$Days += "Tuesday"}
+        if($FullTable[$i].Days -ilike "W") {$Days += "Wednesday"}
+        if($FullTable[$i].Days -ilike "*TH*") {$Days += "Thursday"}
+        if($FullTable[$i].Days -ilike "*F") {$Days += "Friday"}
+
+        $FullTable[$i].Days = $Days
+
+    }
+    return $FullTable
+}
